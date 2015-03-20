b09351a2970f5774cae96aad9bfce4bf7c00d26d
Bruno Pérez <bruno.perez@buddyweb.fr>
2015-03-20 07:29:43 -0400

    initial upload

---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Comming Soon Page</title>
+
+	<!-- JS -->
+	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+	<!-- Twitter Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+</head>
+
+
+<body>
+	<div class="container-fluid">
+
+		<div class="row text-center">
+			<div class="col-xs-8 col-xs-offset-2">
+				<img src="http://fakeimg.pl/350x200/?text=Logo">
+				<h1>Baseline</h1>				
+			</div>
+
+		</div>
+
+		<div class="row text-center">
+			<div class="col-xs-8 col-xs-offset-2">
+				<form class="form-inline">
+					<div class="form-group">
+						<label class="sr-only" for="exampleInputEmail3">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail3" placeholder="Enter email">
+					</div>
+					<button type="submit" class="btn btn-default">Sign in</button>
+				</form>
+				<p>Help Label text info</p>	
+			</div>
+		</div>
+
+
+			
+		</div>
+	</div>
+</body>
+
+</html>
