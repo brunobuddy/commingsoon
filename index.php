6327caf93f77a0e5bc8697d36a5cf27b49b56b4e
Bruno Pérez <bruno.perez@buddyweb.fr>
2015-03-20 11:55:31 -0400

    add mustache PHP

--- index.php
+++ index.php
@@ -1,47 +1,15 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>My Comming Soon Page</title>
+<?php 
 
-	<!-- JS -->
-	<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+// Composer Autoload
+require 'vendor/autoload.php'; 
 
-	<!-- Twitter Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-</head>
+$m = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views'),
+));
 
 
-<body>
-	<div class="container-fluid">
+// loads template from `views/hello_world.mustache` and renders it.
+echo $m->render('home', array('planet' => 'world'));
 
-		<div class="row text-center">
-			<div class="col-xs-8 col-xs-offset-2">
-				<img src="http://fakeimg.pl/350x200/?text=Logo">
-				<h1>Baseline</h1>				
-			</div>
-
-		</div>
-
-		<div class="row text-center">
-			<div class="col-xs-8 col-xs-offset-2">
-				<form class="form-inline">
-					<div class="form-group">
-						<label class="sr-only" for="exampleInputEmail3">Email address</label>
-						<input type="email" class="form-control" id="exampleInputEmail3" placeholder="Enter email">
-					</div>
-					<button type="submit" class="btn btn-default">Sign in</button>
-				</form>
-				<p>Help Label text info</p>	
-			</div>
-		</div>
-
-
-			
-		</div>
-	</div>
-</body>
-
-</html>
+?>
