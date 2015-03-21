a58768e99d5bb1481cfc7cb54e419f85f381aaa9
Bruno Pérez <bruno.perez@buddyweb.fr>
2015-03-21 09:34:30 -0400

    add database connection

--- index.php
+++ index.php
@@ -2,7 +2,24 @@
 <?php 
 
 // Composer Autoload
-require 'vendor/autoload.php'; 
+require 'vendor/autoload.php';
+
+try
+{
+	// Connection base de données
+	$db = new PDO('mysql:host=localhost;dbname=my-comming-soon-page;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$reponse = $db->query('SELECT * FROM page');
+
+$data = $reponse->fetch();
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
 
 $m = new Mustache_Engine(array(
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__) . '/views'),
@@ -10,6 +27,6 @@ $m = new Mustache_Engine(array(
 
 
 // loads template from `views/hello_world.mustache` and renders it.
-echo $m->render('home', array('planet' => 'world'));
+echo $m->render('home', $data);
 
 ?>
